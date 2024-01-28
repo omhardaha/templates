1f7bc5e0c96455e145026c2effdd8fcf10c15371
omhardaha <omprakashhardaha369@gmail.com>
2024-01-28 23:04:22 +0530

    feat(Cart): add basic layout

--- book-shop/src/App.js
+++ book-shop/src/App.js
@@ -9,15 +9,18 @@ import {
 } from "react-router-dom";
 import Home from "./page/Home";
 import Cart from "./page/Cart";
+// import Temp from "./page/Temp";
 
 export default function App() {
     return (
         <>
             <NavBar />
+            <div className="h-16"></div>
             <BrowserRouter>
                 <Routes>
                     <Route path="/" element={<Home />} />
                     <Route path="/cart" element={<Cart />} />
+                    {/* <Route path="/temp" element={<Temp />} /> */}
                 </Routes>
             </BrowserRouter> 
             <Footer />
--- book-shop/src/page/Cart.jsx
+++ book-shop/src/page/Cart.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
 
 const Cart = () => {
-  return (
-    <div>Cart</div>
-  )
+    return (
+        <div className='h-48 bg-blue-100 mx-auto text-larq text-lg'>
+            <div className='p-4 my-1 mx-auto bg-white w-5/6 text-center text-xl'>
+                <div className=''>
+                    <b>Cart</b>
+                </div>
+            </div>
+            <div className='p-4 my-1 mx-auto bg-white w-5/6 '>
+                <div className='flex justify-between '>
+
+                <div className=''>
+                    Deliver to: <b>Jabalpur - 482002</b>
+                </div>
+                <div className='border-2 p-2 rounded-lg '>
+                    <button className='font-bold'>Change</button>
+                </div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
-export default Cart
+export default Cart;
