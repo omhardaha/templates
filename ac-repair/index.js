3f03f37fd268c9c30fc7bb3d30f309ca766bae5a
omhardaha <omprakashhardaha369@gmail.com>
2024-02-05 22:10:53 +0530

    feat:add history and why choose us section

--- ac-repair/index.js
+++ ac-repair/index.js
@@ -1,6 +1,33 @@
+// Navbar Button
 function openNav() {
   document.getElementById("navSidebar").classList.remove("hidden");
 }
 function closeNav() {
   document.getElementById("navSidebar").classList.add("hidden");
 }
+
+
+function addRevealEffect(elements) {
+  const observer = new IntersectionObserver(
+    (entries) => {
+      let revealClass;
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          revealClass = scrollDirection === 'up' ? 'reveal-up' : 'reveal-down';
+
+          entry.target.classList.add(revealClass);
+        } else {
+          entry.target.className = 'subject';
+        }
+      });
+    },
+    { threshold: 0.1 }
+  );
+
+  elements.forEach((element) => {
+    observer.observe(element);
+  });
+}
+
+const elementsToReveal = document.querySelectorAll('.subject');
+addRevealEffect(elementsToReveal);
