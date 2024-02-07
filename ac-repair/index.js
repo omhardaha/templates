303f5fb1b2deac0ce9c063e88698ce8e545786dc
omhardaha <omprakashhardaha369@gmail.com>
2024-02-07 14:03:48 +0530

    chore: remove not used function in index.js

--- ac-repair/index.js
+++ ac-repair/index.js
@@ -5,29 +5,3 @@ function openNav() {
 function closeNav() {
   document.getElementById("navSidebar").classList.add("hidden");
 }
-
-
-function addRevealEffect(elements) {
-  const observer = new IntersectionObserver(
-    (entries) => {
-      let revealClass;
-      entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-          revealClass = scrollDirection === 'up' ? 'reveal-up' : 'reveal-down';
-
-          entry.target.classList.add(revealClass);
-        } else {
-          entry.target.className = 'subject';
-        }
-      });
-    },
-    { threshold: 0.1 }
-  );
-
-  elements.forEach((element) => {
-    observer.observe(element);
-  });
-}
-
-const elementsToReveal = document.querySelectorAll('.subject');
-addRevealEffect(elementsToReveal);
