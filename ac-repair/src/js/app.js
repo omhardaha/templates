4abae672c14f5f2015b1f2643d9887dc355ae921
omhardaha <omprakashhardaha369@gmail.com>
2024-02-10 00:14:42 +0530

    feat: migrate into parcel

---
+++ ac-repair/src/js/app.js
@@ -0,0 +1,8 @@
+// Navbar Button
+function openNav() {
+    document.getElementById("navSidebar").classList.remove("hidden");
+  }
+  function closeNav() {
+    document.getElementById("navSidebar").classList.add("hidden");
+  }
+  
