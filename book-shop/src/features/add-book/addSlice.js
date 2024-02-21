7c2270ae976eaa05db40bdc266b413f95b634796
omhardaha <omprakashhardaha369@gmail.com>
2024-02-21 22:53:42 +0530

    feat:add reducer addBook

--- book-shop/src/features/add-book/addSlice.js
+++ book-shop/src/features/add-book/addSlice.js
@@ -1,20 +1,22 @@
 import { createSlice, nanoid } from '@reduxjs/toolkit'
 
-const initialState = { value: 0 }
+const initialState = {
+  books: [
+    { id: 1234, title: "book1", desc: "This is first book" },
+    { id: 1235, title: "book2", desc: "This is secon book" },
+    { id: 1237, title: "book3", desc: "This is third book" }
+  ]
+}
 
 const counterSlice = createSlice({
   name: 'books',
   initialState,
   reducers: {
-    addBook(state,action) {
-      state.value++
-    },
-    removeBook(state) {
-      state.value--
-    },
-    incrementByAmount(state, action) {
-      state.value += action.payload
-    },
+    addBook(state, action) {
+      const newBook = { id: nanoid(), title: action.payload.title, desc: action.payload.desc };
+      state.books.push(newBook);
+    }
+    
   },
 })
 
--- book-shop/src/pages/Home.jsx
+++ book-shop/src/pages/Home.jsx
@@ -3,6 +3,7 @@ import Header from '../component/home/Header';
 import TopPicks from '../component/home/TopPicks';
 import Quotes from '../component/home/Quotes';
 import BookContainerLg from '../component/container/BookContainerLg';
+import FavBooks from '../component/container/FavBooks';
 // import SinglePage from "../component/Product";
 
 function Home() {
@@ -10,8 +11,10 @@ function Home() {
     <div className="lg:mx-20 mx-10">
       {/* <SinglePage /> */}
       <Header />
+      <FavBooks />
       <TopPicks />
-      
+
+
       <BookContainerLg heading="For Gift Someone" />
       <BookContainerLg heading="Non-Fiction" />
       <Quotes />
